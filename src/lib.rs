108c726b796f40f97eef00dae03fe0660de6cc89
Nicolas Busser <nicolas.busser@lacollection.io>
2024-12-30 10:29:23 +0100

    add printing of snake

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,28 @@
 #![cfg_attr(test, reexport_test_harness_main = "test_main")]
 #![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
 
+extern crate alloc;
+
+use core::borrow::Borrow;
+
 use agb::{
     display::{
         self,
+        object::{OamManaged, Object, Sprite},
         tiled::{RegularMap, TileFormat, TileSetting, TiledMap, VRamManager},
         Priority,
     },
+    fixnum::Vector2D,
     include_background_gfx,
     sound::mixer::Frequency,
 };
 
+use alloc::vec;
+use alloc::vec::Vec;
+
 include_background_gfx!(backgrounds, "121105",
     background => deduplicate "gfx/background.png",
-    grid => deduplicate "gfx/grid.aseprite"
+    grid => deduplicate "gfx/grid.aseprite",
 );
 
 fn create_background(background: &mut RegularMap, vram: &mut VRamManager) {
@@ -71,8 +80,47 @@ fn create_grid(grid: &mut RegularMap, vram: &mut VRamManager) {
     grid.set_visible(true);
 }
 
+static SPRITES: &agb::display::object::Graphics = agb::include_aseprite!("gfx/snake.aseprite");
+
+static SPRITE_HEAD: &Sprite = SPRITES.tags().get("head").sprite(0);
+static SPRITE_BODY: &Sprite = SPRITES.tags().get("body").sprite(0);
+
+type Position = Vector2D<u16>;
+
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+
+struct SnakeBodyCell<'a> {
+    position: Position,
+    sprite: Object<'a>,
+}
+
+const SNAKE_TILE_SIZE: u16 = 16;
+
+struct Snake<'a> {
+    body: Vec<SnakeBodyCell<'a>>,
+    direction: Direction,
+}
+
+impl Snake<'_> {
+    pub fn display(&mut self, objects: &OamManaged) {
+        self.body.iter_mut().for_each(|body_cell| {
+            body_cell.sprite.set_position(Vector2D::<i32> {
+                x: (body_cell.position.x * SNAKE_TILE_SIZE) as i32,
+                y: (body_cell.position.y * SNAKE_TILE_SIZE) as i32,
+            });
+            body_cell.sprite.show();
+        });
+        objects.commit();
+    }
+}
+
 pub fn main(mut gba: agb::Gba) -> ! {
-    let gfx = gba.display.object.get_managed();
+    let objects = gba.display.object.get_managed();
     let vblank = agb::interrupt::VBlank::get();
 
     let (tiled, mut vram) = gba.display.video.tiled0();
@@ -86,19 +134,31 @@ pub fn main(mut gba: agb::Gba) -> ! {
         display::tiled::RegularBackgroundSize::Background32x32,
         TileFormat::FourBpp,
     );
-    let mut foreground = tiled.background(
-        Priority::P1,
-        display::tiled::RegularBackgroundSize::Background32x32,
-        TileFormat::FourBpp,
-    );
 
     let mut mixer = gba.mixer.mixer(Frequency::Hz32768);
     mixer.enable();
+
+    // Init snake
+    let mut snake_starting_body: Vec<SnakeBodyCell> = Vec::new();
+    for i in 0..3 {
+        let sprite = if i == 0 { SPRITE_HEAD } else { SPRITE_BODY };
+
+        snake_starting_body.push(SnakeBodyCell {
+            position: Vector2D { x: 5 - i, y: 5 },
+            sprite: objects.object_sprite(sprite),
+        });
+    }
+    let mut snake = Snake {
+        body: snake_starting_body,
+        direction: Direction::RIGHT,
+    };
 
     loop {
         create_background(&mut background, &mut vram);
         create_grid(&mut background_grid, &mut vram);
 
-        loop {}
+        loop {
+            snake.display(&objects);
+        }
     }
 }
