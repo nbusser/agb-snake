9338a7a7c06efe19133a48be056f7edc2cfde52a
Nicolas Busser <nicolas.busser@lacollection.io>
2024-12-31 11:26:43 +0100

    add input

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@
 
 extern crate alloc;
 
-use core::borrow::Borrow;
-
 use agb::{
     display::{
         self,
@@ -15,11 +13,10 @@ use agb::{
         Priority,
     },
     fixnum::Vector2D,
-    include_background_gfx,
+    include_background_gfx, println,
     sound::mixer::Frequency,
 };
 
-use alloc::vec;
 use alloc::vec::Vec;
 
 include_background_gfx!(backgrounds, "121105",
@@ -83,10 +80,10 @@ fn create_grid(grid: &mut RegularMap, vram: &mut VRamManager) {
 static SPRITES: &agb::display::object::Graphics = agb::include_aseprite!("gfx/snake.aseprite");
 
 static SPRITE_HEAD: &Sprite = SPRITES.tags().get("head").sprite(0);
+static SPRITE_HEAD_DEAD: &Sprite = SPRITES.tags().get("head-dead").sprite(0);
 static SPRITE_BODY: &Sprite = SPRITES.tags().get("body").sprite(0);
 
-type Position = Vector2D<u16>;
-
+#[derive(PartialEq)]
 enum Direction {
     UP,
     DOWN,
@@ -95,27 +92,87 @@ enum Direction {
 }
 
 struct SnakeBodyCell<'a> {
-    position: Position,
+    position: Vector2D<i32>,
     sprite: Object<'a>,
 }
-
-const SNAKE_TILE_SIZE: u16 = 16;
 
 struct Snake<'a> {
     body: Vec<SnakeBodyCell<'a>>,
     direction: Direction,
+    is_alive: bool,
 }
 
 impl Snake<'_> {
+    const SNAKE_TILE_SIZE: i32 = 16;
+    const MIN_X: i32 = 0;
+    const MAX_X: i32 = (display::WIDTH / Snake::SNAKE_TILE_SIZE) - 1;
+    const MIN_Y: i32 = 0;
+    const MAX_Y: i32 = (display::HEIGHT / Snake::SNAKE_TILE_SIZE) - 1;
+
     pub fn display(&mut self, objects: &OamManaged) {
         self.body.iter_mut().for_each(|body_cell| {
             body_cell.sprite.set_position(Vector2D::<i32> {
-                x: (body_cell.position.x * SNAKE_TILE_SIZE) as i32,
-                y: (body_cell.position.y * SNAKE_TILE_SIZE) as i32,
+                x: body_cell.position.x * Snake::SNAKE_TILE_SIZE,
+                y: body_cell.position.y * Snake::SNAKE_TILE_SIZE,
             });
             body_cell.sprite.show();
         });
         objects.commit();
+    }
+
+    pub fn apply_input(&mut self, input: Direction) {
+        if match input {
+            Direction::UP => self.direction != Direction::DOWN,
+            Direction::DOWN => self.direction != Direction::UP,
+            Direction::LEFT => self.direction != Direction::RIGHT,
+            Direction::RIGHT => self.direction != Direction::LEFT,
+        } {
+            self.direction = input;
+        }
+    }
+
+    fn get_movement_offset(direction: &Direction) -> Vector2D<i32> {
+        match direction {
+            Direction::UP => Vector2D::<i32> { x: 0, y: -1 },
+            Direction::DOWN => Vector2D::<i32> { x: 0, y: 1 },
+            Direction::LEFT => Vector2D::<i32> { x: -1, y: 0 },
+            Direction::RIGHT => Vector2D::<i32> { x: 1, y: 0 },
+        }
+    }
+
+    fn head(&self) -> &SnakeBodyCell<'_> {
+        &self.body[0]
+    }
+
+    fn die(&mut self, objects: &OamManaged) {
+        self.is_alive = false;
+        self.body[0]
+            .sprite
+            .set_sprite(objects.sprite(&SPRITE_HEAD_DEAD));
+    }
+
+    pub fn try_move(&mut self, objects: &OamManaged) -> bool {
+        let head_projection = self.head().position + Snake::get_movement_offset(&self.direction);
+
+        if head_projection.x < Snake::MIN_X
+            || head_projection.x > Snake::MAX_X
+            || head_projection.y < Snake::MIN_Y
+            || head_projection.y > Snake::MAX_Y
+            || self
+                .body
+                .iter()
+                .any(|body_cell| body_cell.position == head_projection)
+        {
+            self.die(objects);
+            return false;
+        }
+
+        for i in (1..self.body.len()).rev() {
+            self.body[i].position = self.body[i - 1].position;
+        }
+
+        self.body[0].position = head_projection;
+        return true;
     }
 }
 
@@ -140,7 +197,7 @@ pub fn main(mut gba: agb::Gba) -> ! {
 
     // Init snake
     let mut snake_starting_body: Vec<SnakeBodyCell> = Vec::new();
-    for i in 0..3 {
+    for i in 0..5 {
         let sprite = if i == 0 { SPRITE_HEAD } else { SPRITE_BODY };
 
         snake_starting_body.push(SnakeBodyCell {
@@ -151,14 +208,41 @@ pub fn main(mut gba: agb::Gba) -> ! {
     let mut snake = Snake {
         body: snake_starting_body,
         direction: Direction::RIGHT,
+        is_alive: true,
     };
 
     loop {
         create_background(&mut background, &mut vram);
+
+        let mut input = agb::input::ButtonController::new();
+        loop {
+            input.update();
+            if input.is_just_pressed(agb::input::Button::all()) {
+                break;
+            }
+            vblank.wait_for_vblank();
+        }
+
         create_grid(&mut background_grid, &mut vram);
 
         loop {
-            snake.display(&objects);
+            while snake.is_alive {
+                for _n_frames in 0..30 {
+                    input.update();
+                    if input.is_just_pressed(agb::input::Button::UP) {
+                        snake.apply_input(Direction::UP);
+                    } else if input.is_just_pressed(agb::input::Button::DOWN) {
+                        snake.apply_input(Direction::DOWN);
+                    } else if input.is_just_pressed(agb::input::Button::LEFT) {
+                        snake.apply_input(Direction::LEFT);
+                    } else if input.is_just_pressed(agb::input::Button::RIGHT) {
+                        snake.apply_input(Direction::RIGHT);
+                    }
+                    vblank.wait_for_vblank();
+                }
+                snake.try_move(&objects);
+                snake.display(&objects);
+            }
         }
     }
 }
